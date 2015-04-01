bc16c81c21e2e2a8394af70646363b7e80de58e6
Tone Hoeft <tone@tonehoeft.com>
2015-04-01 22:04:29 +0000

    inital commit

---
+++ js/controllers/EventController.js
@@ -0,0 +1,6 @@
+app.controller('EventController', ['$scope', 'events', '$routeParams', function($scope, events, $routeParams) {
+  $scope.name = 'Tone';
+  events.success(function(data) {
+      $scope.event = data.events[$routeParams.id];
+  });
+}]);
